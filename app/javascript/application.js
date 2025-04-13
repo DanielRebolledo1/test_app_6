0019bb5377591054e14ea850c3a4586bcd4eb187
DaRebolledo <da.rebolledo@duocuc.cl>
2025-04-12 21:41:41 -0400

    Diseño del demo en ruby

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const notice = "<%= notice %>";
+    const alert = "<%= alert %>";
+  
+    if (notice) {
+      Swal.fire({
+        icon: "success",
+        title: "Éxito",
+        text: notice,
+        timer: 3000,
+        showConfirmButton: false,
+      });
+    }
+  
+    if (alert) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: alert,
+        timer: 3000,
+        showConfirmButton: false,
+      });
+    }
+  });
